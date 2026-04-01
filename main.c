7c0e2852f19a5d388815b4ac5e22f7fa360dc848
dearoys <dea.roys@my.jcu.edu.au>
2026-04-02 08:46:03 +1000

    Remove the LED from the code

--- main.c
+++ main.c
@@ -11,10 +11,6 @@
 
 bool manual_mode = true;
 
-#define RED_LED 11
-#define GREEN_LED 12
-#define BLUE_LED 13
-
 #define SPINDLE_PIN 15   // PWM pin for spindle
 
 #define STEPS_PER_MM_X 80.0
@@ -49,7 +45,6 @@ void spindle_init() {
 }
 
 void spindle_set_speed(uint16_t speed) {
-    uint slice = pwm_gpio_to_slice_num(SPINDLE_PIN);
     pwm_set_gpio_level(SPINDLE_PIN, speed);
     spindle_speed = speed;
 }
@@ -58,12 +53,12 @@ void spindle_set_speed(uint16_t speed) {
 void move_x(int step, bool dir){
     current_mode = MOVING;
 
-    gpio_put(xDIR,dir);
+    gpio_put(xDIR, dir);
 
-    for(int i= 0; i<step; i++){
-        gpio_put(xSTEP,1);
+    for(int i = 0; i < step; i++){
+        gpio_put(xSTEP, 1);
         sleep_us(1200);
-        gpio_put(xSTEP,0);
+        gpio_put(xSTEP, 0);
         sleep_us(1200);
     }
 
@@ -74,12 +69,12 @@ void move_x(int step, bool dir){
 void move_y(int step, bool dir){
     current_mode = MOVING;
 
-    gpio_put(yDIR,dir);
+    gpio_put(yDIR, dir);
 
-    for(int i= 0; i<step; i++){
-        gpio_put(ySTEP,1);
+    for(int i = 0; i < step; i++){
+        gpio_put(ySTEP, 1);
         sleep_us(1200);
-        gpio_put(ySTEP,0);
+        gpio_put(ySTEP, 0);
         sleep_us(1200);
     }
 
@@ -90,12 +85,12 @@ void move_y(int step, bool dir){
 void move_z(int step, bool dir){
     current_mode = MOVING;
 
-    gpio_put(zDIR,dir);
+    gpio_put(zDIR, dir);
 
-    for(int i= 0; i<step; i++){
-        gpio_put(zSTEP,1);
+    for(int i = 0; i < step; i++){
+        gpio_put(zSTEP, 1);
         sleep_us(1200);
-        gpio_put(zSTEP,0);
+        gpio_put(zSTEP, 0);
         sleep_us(1200);
     }
 
@@ -107,16 +102,16 @@ void move_z(int step, bool dir){
 void handle_manual_mode(int ch) {
     switch(ch){
         // X axis
-        case 'a': move_x(1000,false); break; // left
-        case 'd': move_x(1000,true);  break; // right
+        case 'a': move_x(1000, false); break;
+        case 'd': move_x(1000, true);  break;
 
         // Y axis
-        case 'w': move_y(1000,false); break; // up
-        case 's': move_y(1000,true);  break; // down
+        case 'w': move_y(1000, false); break;
+        case 's': move_y(1000, true);  break;
 
         // Z axis
-        case 'q': move_z(1000,false); break; // back
-        case 'e': move_z(1000,true);  break; // forward
+        case 'q': move_z(1000, false); break;
+        case 'e': move_z(1000, true);  break;
 
         // Spindle speed
         case '+':
@@ -135,7 +130,7 @@ void handle_manual_mode(int ch) {
     }
 }
 
-// ---------------- COMMAND MODE (placeholder) ----------------
+// ---------------- COMMAND MODE ----------------
 void handle_command_mode(int ch) {
     // Future G-code or structured command parser
 }
@@ -144,24 +139,11 @@ int mm_to_steps(float mm, float steps_per_mm) {
     return (int)(mm * steps_per_mm);
 }
 
-float x_mm = 10.0;
-int steps = mm_to_steps(x_mm, STEPS_PER_MM_X);
-move_x(steps, true);
-
 // ---------------- MAIN ----------------
 int main(void)
 {
     stdio_init_all();
     mmhal_init();
-
-    // LEDs
-    gpio_init(RED_LED);
-    gpio_init(GREEN_LED);
-    gpio_init(BLUE_LED);
-
-    gpio_set_dir(RED_LED, true);
-    gpio_set_dir(GREEN_LED, true);
-    gpio_set_dir(BLUE_LED, true);
 
     // Stepper pins
     gpio_init(xSTEP); gpio_init(xDIR);
@@ -182,17 +164,9 @@ int main(void)
 
         if (ch != PICO_ERROR_TIMEOUT){
 
-            // Toggle mode with single key
+            // Toggle mode (no LEDs now)
             if (ch == 'm') {
                 manual_mode = !manual_mode;
-
-                if (manual_mode) {
-                    gpio_put(GREEN_LED, 1);
-                    gpio_put(RED_LED, 0);
-                } else {
-                    gpio_put(RED_LED, 1);
-                    gpio_put(GREEN_LED, 0);
-                }
                 continue;
             }
 
